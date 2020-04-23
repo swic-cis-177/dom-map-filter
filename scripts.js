910ed4500732a5ab540ec4240b7eef3250791971
Manav Misra <manavm@visionwebsoft.com>
2020-04-22 22:24:58 -0500

    :seedling: Set up HW

---
+++ scripts.js
@@ -0,0 +1,31 @@
+import { doc } from "prettier";
+
+const name = document.querySelector("input");
+const names = [];
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
+
+document.querySelector("form").addEventListener("submit", e => {
+  e.preventDefault();
+
+  // TODO: 'push' 'name.value' onto 'names'
+});
+
+document.querySelector("#no-long").addEventListener("click", () => {
+  // TODO: FILTER out 'names' longer than 5 characters
+  // HINT: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length
+});
+
+document.querySelector("#uppercase").addEventListener("click", () => {
+  // TODO: Make all 'names' ALL CAPS!
+  // HINT: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+});
+
+document.querySelector("#get-random").addEventListener("click"() => {
+  // TODO: Replace the 'textContent' for the 'p' tag with a random name from 'names'.
+  // HINT: Use 'getRandomIntInclusive' with a 'max' of 'names.length - 1'.
+})
